380b79b8e7e796cda12f8c15532043cf43367a5a
Guilherme Nunes <guilherme.augd@gmail.com>
2026-01-20 16:33:20 -0300

    swapped preset maritaca model to groq

--- my_keys.py
+++ my_keys.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-MARITACA_API_KEY = os.getenv("MARITACA_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
