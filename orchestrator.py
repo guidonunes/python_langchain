160024adb494ac82f716903b777dd7e6fc3c5c65
Guilherme Nunes <guilherme.augd@gmail.com>
2026-01-26 10:55:21 -0300

    implement orchestrator logic

--- orchestrator.py
+++ orchestrator.py
@@ -5,4 +5,28 @@ from langchain_core.globals import set_debug
 set_debug(False)
 
 from langchain import hub
-from langchain.agents import create_react_agent, AgentExecutor
+from langchain.agents import create_react_agent
+from langchain.tools import tool
+
+
+class Orchestrator:
+    def __init__(self):
+        self.llm = ChatGoogleGenerativeAI(
+            model=GEMINI_FLASH,
+            api_key=GEMINI_API_KEY
+        )
+
+        market_data_tool = None
+
+        self.tools = [
+            tool(
+                name= market_data_tool.name,
+                func= market_data_tool.run,
+                description= market_data_tool.description,
+                return_direct = market_data_tool.return_direct
+            )
+        ]
+
+        prompt = hub.pull("hwchase17/react")
+
+        self.agent = create_react_agent(self.llm, self.tools, prompt)
