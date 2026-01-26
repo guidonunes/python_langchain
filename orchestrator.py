7fc1699bc17d7317e7112c24e495eb4813854532
Guilherme Nunes <guilherme.augd@gmail.com>
2026-01-26 09:50:43 -0300

    add orchestrator and imports

---
+++ orchestrator.py
@@ -0,0 +1,5 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from my_models import GEMINI_FLASH, GROQ_MODEL
+from my_keys import GEMINI_API_KEY, GROQ_API_KEY
+from langchain_core.globals import set_debug
+set_debug(False)
